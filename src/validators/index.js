8506862786e99153340393c30adb40149d3e5462
Likhitha856 <sanjana852006@gmail.com>
2026-08-25 12:04:59 +0530

    add project validators

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,4 +1,5 @@
 import { body } from "express-validator";
+import { AvailableUserRole } from "../utils/constants.js";
 
 const userRegisterValidator = () => {
   return [
@@ -57,14 +58,43 @@ const userForgotPasswordValidator = () => {
       .withMessage("Email is invalid"),
   ];
 
-const userResetForgotPasswordValidator = () => {
-  return [body("newPassword").notEmpty().withMessage("password is required")];
+  const userResetForgotPasswordValidator = () => {
+    return [body("newPassword").notEmpty().withMessage("password is required")];
+  };
 };
+
+const userResetForgotPasswordValidator = () => {
+  return [body("newPassword").notEmpty().withMessage("Password is required")];
+};
+
+const createProjectValidator = () => {
+  return [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("description").optional(),
+  ];
+};
+
+const addMemberToProjectValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("role")
+      .notEmpty()
+      .withMessage("Role is required")
+      .isIn(AvailableUserRole)
+      .withMessage("Role is invalid"),
+  ];
 };
 export {
   userRegisterValidator,
   userLoginValidator,
   userChangeCurrentPasswordValidator,
   userForgotPasswordValidator,
-  userResetForgotPasswordValidator
+  userResetForgotPasswordValidator,
+  createProjectValidator,
+  addMemberToProjectValidator,
 };
